81fd26eca967af9a9818b25a8a7da7344f8346ba
nhamilton1 <nickh3592@gmail.com>
2021-09-30 12:44:10 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+console.log(`web46 rulez`)
+console.log(process.argv[2])
+console.log(process.argv[3])
+console.log(process.env.username)
+console.log(process.env.FOO)
+
+const express = require('express')
+
+const server = express()
+server.use(express.json())
+
+server.get('/hello', (req, res) => {
+    res.send('<h1>Hello there</h1>')
+})
+
+const port = process.env.port || 3000 // this is where the port number is in heroku
+
+server.listen(port, () => {
+    console.log(`listening on ${port}`)
+})
